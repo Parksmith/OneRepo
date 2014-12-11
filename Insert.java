89bc5255cea8617ccebd3097698505f64a18b057
Taylor <fomemail@163.com>
2014-12-11 08:57:22 +0000

    Update Insert.java

--- Insert.java
+++ Insert.java
@@ -1,12 +1,12 @@
 public void Insertsort(int[] a){
-	int i,j;
+    int i,j;
 	for (i = 1; i < a.length; i++) {
 	  if (a[i]<a[i-1]) {
 			int temp=a[i];
 			for (j = i-1; j >=0 && a[j]>temp; j--) {
 			  a[j+1]=a[j];
 			}
-				a[j+1]=temp;
+			a[j+1]=temp;
 		}
 	}
 }
